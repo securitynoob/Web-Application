e6e7cafbf866ce7c4736658e757b50ba3f3597c8
Likhith Anvhesh Daasa <47549351+securitynoob@users.noreply.github.com>
2019-10-26 23:07:27 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+if __name__ == "__main__":
+    app.run()
+    
+
+
+# In[ ]:
+
+
+
+
