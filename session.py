d286b74e8d6b2447f6529c9d9bd488618f87578e
Likhith Anvhesh Daasa <47549351+securitynoob@users.noreply.github.com>
2019-10-26 23:07:40 -0400

    Create session.py

---
+++ session.py
@@ -0,0 +1,45 @@
+from flask import Flask, session, render_template, request, redirect, g
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	if request.method == 'POST':
+		session.pop('user', None)
+
+		if request.form['password'] == 'password':
+			session['user'] = request.form['username']
+			return redirect(url_for('mainpage'))
+
+	return render_template('signin.html')
+
+@app.route('/mainpage')
+def mainpage():
+	if g.user:
+		return render_template('mainpage.html')
+
+	return redirect(url_for('signin'))
+
+@app.before_request
+def before_request():
+	g.user = None
+	if 'user' in session:
+		g.user = session['user']
+
+@app.route('/getsession')
+def getsession():
+	if 'user' in session:
+		return session['user']
+
+	return 'User not logged in!'
+
+@app.route('/dropsession')
+def dropsession():
+	session.pop('user', None)
+	return 'Dropped!'
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
